9232cc6861f946be265cbc11d965f169ba3b75c1
damdaris <darisadam@upi.edu>
2024-09-14 22:32:10 +0700

    Initial Commit

---
+++ SeaCreatures/SeaCreaturesApp.swift
@@ -0,0 +1,26 @@
+//
+//  SeaCreaturesApp.swift
+//  SeaCreatures
+//
+//  Created by Adam Daris Ryadhi on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SeaCreaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowResizability(.contentSize)
+        .defaultSize(width: 0.75, height: 0.5, depth: 0.5, in: .meters)
+        
+        WindowGroup(id: "creatureWindow", for: String.self) { $modelName in
+            SeaCreatureDetailView(modelName: modelName ?? "ToyBiplane")
+                .padding3D([.back, .top], 250)
+        }
+        .windowStyle(.volumetric)
+        .defaultSize(width: 0.5, height: 0.5, depth: 0.5, in: .meters)
+    }
+}
---
+++ SeaCreatures/SeaScreature.swift
@@ -0,0 +1,14 @@
+//
+//  SeaScreature.swift
+//  SeaCreatures
+//
+//  Created by Adam Daris Ryadhi on 11/07/24.
+//
+
+import Foundation
+
+struct SeaCreature: Identifiable {
+    var name: String
+    var modelName: String
+    var id = UUID()
+}
